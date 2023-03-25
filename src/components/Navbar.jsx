6c69cc1e8029252c5f029418e7f047d8a4095893
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-25 16:42:48 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {styles} from '../styles'
-import {navLink } from '../constants'
-import {logo, menu , close} from '../assets'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+  const [active , setActive] = useState("");
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center
+  py-5 fixed top-0 z-20`}
+    >
+      <div
+        className="w-full flex justify-center items-center 
+    max-w-7xl mx-auto"
+      >
+        <Link to="/" 
+        className="flex items-center gap-2"
+        onClick={()=>{setActive("");
+        window.scrollTo(0,0)}}></Link>
+        <img src={logo} alt="" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
