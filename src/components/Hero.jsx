3f6deb6af6e2db07b521344c8bf66eaee4bb641d
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-26 15:50:11 +0530

    created in hero component a name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import { styles } from '../styles'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 // import {ComputersCanvas} from './canvas'
 
 const Hero = () => {
   return (
-   <section className="relative w-full h-screen mx-auto">
-   <div>
-    
-   </div>
+    <section className="relative w-full h-screen mx-auto">
+    <div className={`${styles.paddingX} absolute inset-0 top-[120px] mx-w-7xl
+    mx-auto flex flex-row items-start gap-5`}>
+       <div className="flex flex-col justify-centter items-cener mt-5">
+       <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+       <div className="w-1 sm:h-80 h-40 voilet-gradient"/>
 
-   </section>
-  )
-}
+       </div>
+       <div>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi I'm <span className="text-[#915eff] font-serif">Deependra Singh</span></h1>
+       </div>
+    </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
